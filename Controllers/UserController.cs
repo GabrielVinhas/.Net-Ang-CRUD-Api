cb3d365221ee7522e95af4c1f17b82e0f361829d
Gabriel Vinhas <105182274+GabrielVinhas@users.noreply.github.com>
2023-08-14 10:52:53 -0300

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            User user = await _context.Users.FindAsync(id);
+
+            if(user == null)
+                return NotFound();
+
+            return user;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<User>>> UpdateUser(User user) 
+        {
+            var dbUser = await _context.Users.FindAsync(user.Id);
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            dbUser.Name = user.Name;
+            dbUser.LastName = user.LastName;
+            dbUser.Age = user.Age;
+            dbUser.Occupation = user.Occupation;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int id) 
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserAPI
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
+        public string Occupation { get; set; } = string.Empty;
+    }
+}
